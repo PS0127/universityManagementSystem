762d5d330efd1cafcfc336eab7c3bc93347de6a0
Patryk <Patryksysa2@gmail.com>
2023-06-22 17:59:02 +0100

    Add project files.

---
+++ Pages/Students/CreateStudent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace universityManagementSystem.Pages.Students
+{
+    public class CreateStudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Students/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace universityManagementSystem.Pages.Students
+{
+    public class StudentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
